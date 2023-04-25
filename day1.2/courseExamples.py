7729b90bc1acd0d58237fd8a8bc5e60cecad125c
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2023-04-25 19:30:50 -0300

    Feat: columnName, studentsFilter

--- day1.2/courseExamples.py
+++ day1.2/courseExamples.py
@@ -19,3 +19,24 @@ def infiniteSum():
     print(sum)
 
 #----------------------------------------------------------------------------------------------------
+
+# Dado um arquivo contendo estudantes e suas respectivas notas, escreva um programa que:
+# lê todas essas informações;
+# aplique um filtro, mantendo somente as pessoas que estão reprovadas;
+# escreva seus nomes em outro arquivo.
+# Considere que a nota miníma para aprovação é 6.
+def filterStudents():
+    os.system('clear')
+    gradeToPass = 6
+    filteredStud = {}
+    file = open('students.txt', mode="r")
+    studentList = file.read().split('\n')
+    for student in studentList:
+        studentInfo = student.split(' ')
+        studentName = studentInfo[0]
+        studentGrade = studentInfo[1] # Pega a nota do array
+        if (int(studentGrade) >= gradeToPass):
+            filteredStud[studentName] = studentGrade
+    print((filteredStud))
+
+os.system('clear')
